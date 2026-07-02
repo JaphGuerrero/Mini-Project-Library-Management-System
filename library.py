9f15edd8e4153020ef4c57acca8f8ffc52bdc757
O.Guerrero <159329289+JaphGuerrero@users.noreply.github.com>
2026-07-01 21:50:21 -0400

    Create library.py
    Book and library classes defined along with their respective methods.

---
+++ library.py
@@ -0,0 +1,34 @@
+#A simple library management system in Python
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.is_available = True
+        
+    def borrow_book(self):
+        pass
+    
+    def return_book(self):
+        pass
+    
+    def display_info(self):
+        pass
+    
+
+class Library:
+    def __init__(self, books):
+        self.books = books
+        
+    def add_book(self, book):
+        pass
+    
+    def show_book(self):
+        pass
+    
+    def boorrow_book(self, title):
+        pass
+    
+    def return_book(self, title):
+        pass
+        
